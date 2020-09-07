f9f25207680f9b8d2b6515f322e0c01f78cae19f
Rohit <rohitjakhar940@gmail.com>
2020-09-07 12:49:02 +0530

    Make simple version

---
+++ app/src/main/java/com/easymsging/android/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.easymsging.android
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.telephony.PhoneNumberUtils
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+       // supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
+        super.onCreate(savedInstanceState)
+        val num = PhoneNumberUtils.getNumberFromIntent(intent,this)
+        setContentView(R.layout.activity_main)
+        edNumber.requestFocus()
+
+        if (!num.isNullOrEmpty()){
+            edNumber.setText(num)
+            edNumber.nextFocusDownId
+        }
+
+            button2.setOnClickListener {
+                val cntryCode: String = edCode.text.toString().trim()
+                val number: String = edNumber.text.toString().trim()
+                if (cntryCode.isNullOrBlank()) {
+                    edCode.error = "Enter Country Code"
+                    return@setOnClickListener
+                }
+                if (number.isNullOrBlank()) {
+                    edNumber.error = "Enter  Mobile Number"
+                    return@setOnClickListener
+                }
+
+                val mobile:String = cntryCode+number
+                val message:String = edText.text.toString()
+
+                val link: String = "https://api.whatsapp.com/send?phone=$mobile&text=$message"
+
+                val intent = Intent(Intent.ACTION_VIEW)
+                intent.setData(Uri.parse(link))
+
+                startActivity(Intent.createChooser(intent, null))
+            }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+    }
+}
